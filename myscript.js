74df6b1b0c4ff6c82c716304574127acc9215e27
DonavanC13 <donavanc13@outlook.com>
2024-04-17 17:30:07 -0400

    finished function allows for a single round of the game to be played

--- myscript.js
+++ myscript.js
@@ -4,16 +4,24 @@ function getComputerChoice() {
     return choices[randomIndex];
  }
  const computerSelection = getComputerChoice();
- console.log("Computer selected:", 
-computerSelection)
 const playerSelection = "rock"
-console.log("Player selected:", playerSelection)
 
 function playRound(playerSelection, computerSelection){
     console.log("Player selected:", playerSelection)
     console.log("Computer selected:", computerSelection);
     if (playerSelection === computerSelection){
         return "It's a tie!";
+    } else if (
+        (playerSelection === "rock" && 
+    computerSelection === "scissors") || 
+    (playerSelection === "paper" && 
+computerSelection === "rock") ||
+(playerSelection === "scissors" && 
+computerSelection === "paper")
+    ){
+        return "You win!";
+    } else {
+        return "You lose!";
     }
 
 }
