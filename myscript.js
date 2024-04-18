72d586a7c40540794df20365a8cf429a8c65dce9
DonavanC13 <donavanc13@outlook.com>
2024-04-18 15:31:02 -0400

    created function for game rules created a function to set the basis of game rules

--- myscript.js
+++ myscript.js
@@ -1,3 +1,26 @@
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = 
+        prompt("Enter your choice(rock, paper, or scissors):").toLowerCase();
+        const computerSelection = getComputerChoice();
+        console.log("Round", i + 1);
+        console.log("Player selected:", playerSelection);
+        console.log("Computer selected:", computerSelection);
+        
+        const result = playRound(playerSelection, computerSelection);
+        console.log(result);
+        if (result === "You win!") {
+            playerScore++;
+        } else if (result === "You lose!")
+    {computerScore++;}
+  }
+}
+
+console.log("Final scores:");
+console.log()
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random()* choices.length);
@@ -23,5 +46,4 @@ computerSelection === "paper")
     } else {
         return "You lose!";
     }
-
 }
