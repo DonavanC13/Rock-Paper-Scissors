7add83efb858ab30be31876a3812aa0f12579355
DonavanC13 <donavanc13@outlook.com>
2024-05-13 13:40:37 -0400

    added buttons, reset js file to original code

--- myscript.js
+++ myscript.js
@@ -3,10 +3,10 @@ function playGame() {
     let playerScore = 0;
     let computerScore = 0;
 
-    //for (let i = 0; i < 5; i++) {
-    //    const playerSelection = 
-    //    prompt("Enter your choice(rock, paper, or scissors):").toLowerCase();
-    //    const computerSelection = getComputerChoice();
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = 
+        prompt("Enter your choice(rock, paper, or scissors):").toLowerCase();
+        const computerSelection = getComputerChoice();
         console.log("Round", i + 1);
        console.log("Player selected:", playerSelection);
        console.log("Computer selected:", computerSelection);
